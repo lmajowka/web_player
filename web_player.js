ea4b41591cbeeb98f351ad355472bdf823585601
lmajowka <leo.majowka@gmail.com>
2017-11-13 20:13:25 +0200

    make it simpler and remove jQuery dependency

--- web_player.js
+++ web_player.js
@@ -1,26 +1,26 @@
-	var y = 0;
-	var speed = 50;
-	var timer = null;
+		var speed = 50;
+		var timer = null;
 
-	function webPlayerScroll(){
-		$(document).scrollTop(y,0);
-		timer = setTimeout(webPlayerScroll, speed);
-		y++;
-	}
+		function webPlayerScroll(){
+			window.scrollBy(0,1)
+			timer = setTimeout(webPlayerScroll, speed);
+		}
 
-	function renderPanel(){
-		$('body').append('<div id="web-player-panel" style="position:fixed;top:10px;right:10px;border:1px solid red"><div onclick="webPlayerStart()">START</div><div onclick="webPlayerStop()">PAUSE	</div></div>')
-	}
+		function renderPanel(){
+			var div = document.createElement('div')
+			div.innerHTML = '<div onclick="webPlayerStart()">START</div><div onclick="webPlayerStop()">PAUSE</div>';
+			div.setAttribute('id', 'web-player-panel');
+			div.setAttribute('style', 'position:fixed;top:10px;right:10px;border-radius: 23%;background: #CCC;color: #FFF;');
+			document.getElementsByTagName('body')[0].appendChild(div);
+		}
 
-	function webPlayerStart(){
-		webPlayerScroll();
-	}
+		function webPlayerStart(){
+			webPlayerScroll();
+		}
 
-	function webPlayerStop(){
-		clearTimeout(timer);
-	}
+		function webPlayerStop(){
+			clearTimeout(timer);
+		}
 
 
-	renderPanel();
-
-				
+		renderPanel();
